7a1e69cf097c9e5d9cf7cf9158927a91ecb1ff26
Linet <teweilinet@gmail.com>
2024-05-21 23:41:15 +0300

    python sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length <= 0:
+        print([])  
+        return
+    
+    fibonacci_sequence=[0, 1]  
+    for n in range(2,length):
+        next_value = fibonacci_sequence[n - 1] + fibonacci_sequence[n - 2]
+        fibonacci_sequence.append(next_value)
+    
+    print(fibonacci_sequence[:length]) 
     pass
